fe422670d7ec9ab7df8b253128574f09c5924e35
Zhengtao Jiang <2876253980@qq.com>
2025-07-26 23:37:46 +0800

    Update database configuration to use new MySQL host, database name, username, and password.

--- config/database.php
+++ config/database.php
@@ -3,10 +3,10 @@ declare (strict_types=1);
 
 return [
     'driver' => 'mysql',
-    'host' => '127.0.0.1',
-    'database' => 'demo',
-    'username' => 'demo',
-    'password' => 'TbfXmL2JTcXYYrWZ',
+    'host' => 'mysql.zeabur.internal',
+    'database' => 'zeabur',
+    'username' => 'root',
+    'password' => 'HA50yL2c6XNK4ht1bonRUY739GqxJT8I',
     'charset' => 'utf8mb4',
     'collation' => 'utf8mb4_unicode_ci',
     'prefix' => 'acg_',
